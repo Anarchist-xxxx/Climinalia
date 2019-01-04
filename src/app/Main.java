abb0c567276c409e587a9eb7ebd494cf46ff7b4b
Anarchist-xxxx <neveranarchy@gmail.com>
2019-01-04 19:58:25 +0900

    iconの追加

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,15 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("frame.fxml"));
         primaryStage.setTitle("Criminalia");
+
+        //アイコンを設定するよ
+        try {
+            Image icon = new Image(getClass().getResourceAsStream("/icon.png"));
+            primaryStage.getIcons().add(icon);
+        } catch(Exception e ) {
+            e.printStackTrace();
+        }
+
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
